e3cc527f665de4c24dbca2442cccdbdf9534b7bf
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-07-03 09:33:21 +0300

    format

--- src/main/java/com/example/cours/repository/CourseRepository.java
+++ src/main/java/com/example/cours/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.example.cours.repository;
 
 import com.example.cours.entity.Course;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface CourseRepository extends JpaRepository<Course, UUID> {
-}
+public interface CourseRepository extends JpaRepository<Course, UUID> {}
