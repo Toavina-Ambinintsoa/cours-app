0410f45b18a6e02e3c38be87620c60e048cf289c
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-07-06 07:56:50 +0300

    feat: subscription endpoint- rename user to juser

--- src/main/java/com/example/cours/repository/JUserRepository.java
+++ src/main/java/com/example/cours/repository/JUserRepository.java
@@ -1,9 +1,9 @@
 package com.example.cours.repository;
 
-import com.example.cours.entity.User;
+import com.example.cours.entity.JUser;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface JUserRepository extends JpaRepository<JUser, UUID> {}
